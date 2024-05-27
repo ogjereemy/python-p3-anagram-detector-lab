9e92672e8b8795f6b8db3825bbb737a95e153079
ogola <ogolajeremy601@gmail.com>
2024-05-27 11:31:57 +0300

    adds anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+
+# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        new_anagrams = []
+        
+        word_list = sorted([char for char in self.word])
+        for word in list:
+            if sorted([letter for letter in word]) == word_list:
+                new_anagrams.append(word)
+        return new_anagrams
